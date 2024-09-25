cf025ef6612b3b37a5fabb8851e082882c3d9521
tanishaasib <tanishaassii@gmail.com>
2024-09-26 00:37:53 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -6,12 +6,20 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     mumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com',
+      url: 'https://rpc.ankr.com/polygon_mumbai',
       accounts: [process.env.PRIVATE_KEY],
     },
     goerli: {
       url: 'https://ethereum-goerli.publicnode.com',
       accounts: [process.env.PRIVATE_KEY],
     },
+    sepolia:{
+      url: 'https://ethereum-sepolia-rpc.publicnode.com',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    amoy:{
+      url: 'https://rpc-amoy.polygon.technology/',
+      accounts: [process.env.PRIVATE_KEY],
+    }
   }
 };
