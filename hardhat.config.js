4a813ccf0fbc3f7d9443072526d736048dd06507
tanishaasib <tanishaassii@gmail.com>
2024-09-26 00:38:16 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,14 +5,6 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    mumbai: {
-      url: 'https://rpc.ankr.com/polygon_mumbai',
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    goerli: {
-      url: 'https://ethereum-goerli.publicnode.com',
-      accounts: [process.env.PRIVATE_KEY],
-    },
     sepolia:{
       url: 'https://ethereum-sepolia-rpc.publicnode.com',
       accounts: [process.env.PRIVATE_KEY],
