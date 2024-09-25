8a3437dab01bdbef4aaad296fac2ea9b62c68ec6
tanishaasib <tanishaassii@gmail.com>
2024-09-26 00:30:37 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -1,26 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
+const tokenContractJSON = require("../artifacts/contracts/Dinosaur.sol/Dinosaur.json");
 
-const tokenAddress = ""; // place your erc20 contract address here
+const tokenAddress = "0xaD7B479e0b34A1AeefF5b5994959912e87D9671c";
 const tokenABI = tokenContractJSON.abi;
-const walletAddress = ""; // place your public address for your wallet here
+const walletAddress = "0x685eB3c7f8bb318e6f79aE12C1BCDDa636c2276f"; 
 
 async function main() {
-
     const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
 
-    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
-  }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
+    console.log("Dinosaur attedning school amoy balance: " + await token.balanceOf(walletAddress) + " fxDS");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
-  });
+});
